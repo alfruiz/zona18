9129cf41f1ef7ba69eb6b26ec4d5019842c141b0
Alfonso Ruiz <aae000q@email.wal-mart.com>
2018-05-03 11:39:31 -0500

    update styles

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 /**
  * component TODO
@@ -12,39 +12,72 @@ import React from 'react';
  * @return {JSX}
  * @author Alfonso Ruiz alfonso.ruiz@walmart.com
  */
-const Todo = ({ handleComplete, handleModify, handleChange, index, ...todo }) => {
-    let todoView;
-    if (todo.toModify) {
-        todoView = [
-            <span key="todo-input">
-                <input type="text" value={todo.label} onChange={(event) => { handleChange(event, index); }} />
-            </span>
-        ];
-    } else {
-        todoView = [
-            <span
-                key="todo-input"
-                style={{ 'textDecoration': `${todo.complete ? 'line-through' : 'none'}` }}
-                onClick={() => { handleComplete(index); }}>
-                {todo.label}
-            </span>
-        ]
+class Todo extends Component {
+    state = {
+        hover: false,
+    };
 
+    handleHover = () => {
+        const hover = !this.state.hover;
+        this.setState({ hover });
     }
 
-    todoView.unshift(
-        <span key="todo-button">
-            <button onClick={() => { handleModify(index); }}>C</button>
-        </span>
-    );
+    render() {
 
-    return (
-        <li>
-            <div>
-                {todoView}
-            </div>
-        </li>
-    );
+        const { handleComplete, handleModify, handleChange, index, ...todo } = this.props;
+
+        let todoView;
+        if (todo.toModify) {
+            todoView = [
+                <span key="todo-input">
+                    <input type="text" value={todo.label} onChange={(event) => { handleChange(event, index); }} />
+                </span>
+            ];
+        } else {
+            let style = {
+                style:
+                    {
+                        'textDecoration':
+                            `${
+                            todo.complete ?
+                                'line-through' :
+                                (
+                                    this.state.hover ?
+                                        'underline' :
+                                        'none'
+                                )
+                            }`
+                    }
+            };
+
+            todoView = [
+                <span
+                    key="todo-input"
+                    {...style}
+                    onClick={() => { handleComplete(index); }}
+                    onMouseEnter={this.handleHover}
+                    onMouseLeave={this.handleHover}
+                >
+                    {todo.label}
+                </span>
+            ]
+
+        }
+
+        todoView.unshift(
+            <span key="todo-button">
+                <button onClick={() => { handleModify(index); }}>C</button>
+            </span>
+        );
+
+        return (
+            <li>
+                <div>
+                    {todoView}
+                </div>
+            </li>
+        );
+    }
 }
 
 export default Todo;
